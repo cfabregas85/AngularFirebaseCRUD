fc3260b5671aaf2bd0a4694d711bf75a7ec5b335
Carlos <ing.cfabregas@gmail.com>
2019-08-21 04:58:36 -0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonComponent } from './pages/person/person.component';
+import { PeopleComponent } from './pages/people/people.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonComponent,
+    PeopleComponent
   ],
   imports: [
     BrowserModule,
