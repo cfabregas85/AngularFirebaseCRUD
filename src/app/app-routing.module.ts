d461792662349a837e9baae1d957b947d3e01084
Carlos <ing.cfabregas@gmail.com>
2019-09-04 06:16:31 -0700

    Implemented people.component.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PeopleComponent } from './pages/people/people.component';
+import { PersonComponent } from './pages/person/person.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'people', component:PeopleComponent },  
+  { path:'person/:id', component:PersonComponent },
+  { path:'**', pathMatch:'full', redirectTo: 'people'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
